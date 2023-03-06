90ae7aa919e7ef7dd8cc94cacf9342627d23cc04
flaviopagano <119114629+flaviopagano@users.noreply.github.com>
2023-03-06 17:19:19 +0100

    Add files via upload

---
+++ First API - 01/src/main/java/co/develhope/spingBootStart/controllers/NameController.java
@@ -0,0 +1,30 @@
+package co.develhope.spingBootStart.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NameController {
+
+    @GetMapping("/name")
+    public String getName() {
+        return "John";
+    }
+
+    @PostMapping("/reversedName")
+    public String getReversedName(){
+        String name = "John";
+        StringBuilder reversedName = new StringBuilder(name).reverse();
+        return reversedName.toString();
+    }
+
+    /**
+     * questi metodi di GetMapping e
+     * PostMapping li ho trovati su chatGPT
+     */
+
+
+
+}
---
+++ First API - 01/src/test/java/co/develhope/spingBootStart/SpingBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.spingBootStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
