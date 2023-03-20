cf0db06605219e323bfa1b482fc3925bb3acbab2
flaviopagano <119114629+flaviopagano@users.noreply.github.com>
2023-03-20 11:51:49 +0100

    Add files via upload

---
+++ Spring Repositories - 01/src/test/java/com/example/Spring/Repositories1/SpringRepositories01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Repositories1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepositories01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
