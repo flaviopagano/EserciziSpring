e2b2dfa0605cdd673fc97ab01f23068cbd9dc5b5
flaviopagano <119114629+flaviopagano@users.noreply.github.com>
2023-04-05 11:25:37 +0200

    Delete Custom Queries - 01 directory

--- Custom Queries - 01/src/main/java/co/develhope/customqueries1/entities/FlightStatus.java
+++
@@ -1,7 +0,0 @@
-package co.develhope.customqueries1.entities;
-
-public enum FlightStatus {
-    ON_TIME,
-    DELAYED,
-    CANCELLED;
-}
--- Custom Queries - 01/src/main/java/co/develhope/customqueries1/repositories/FlightRepository.java
+++
@@ -1,7 +0,0 @@
-package co.develhope.customqueries1.repositories;
-
-import co.develhope.customqueries1.entities.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightRepository extends JpaRepository<Flight, Long> {
-}
